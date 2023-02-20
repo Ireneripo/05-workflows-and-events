4f2da28ea311d2f2b4d037318d036957e48a231e
Irene Riportella <riportellairene@gmail.com>
2023-02-20 17:21:45 +0100

    add new comment [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // My tests
+// Add yet another comment!
 
 describe('MainContent', () => {
   it('should render a button', () => {
